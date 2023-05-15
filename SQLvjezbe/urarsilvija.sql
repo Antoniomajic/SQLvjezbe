25bd820c17b867f6641130e98fe769e1b874389a
Antoniomajic <antonio.majich@gmail.com>
2023-05-15 14:23:53 +0200

    Modified -urarsilvija.sql- & added new ER diagram

--- SQLvjezbe/urarsilvija.sql
+++ SQLvjezbe/urarsilvija.sql
@@ -1,37 +1,44 @@
---create database urarsilvija;
---use urarsilvija;
+use master;
+drop database if exists urarsilvija;
+create database urarsilvija;
+use urarsilvija;
 
-create table obrt(
-	naziv varchar(50),
-	adresa varchar(100),
-	email varchar(100),
-	telefon varchar(20),
-	iban char(21)
-);
-
-create table urar(
-	oib char(11),
-	certificiran bit,
-	pocetakradnogvremena time,
-	krajradnogvremena time
-);
-
-create table usluga(
-	cijena decimal(18,2),
-	trajanje int,
-	opis varchar(50)
+create table korisnik(
+	id int not null primary key identity(1,1),
+	ime varchar(50),
+	prezime varchar(50),
+	telefon varchar(20)
 );
 
 create table sat(
+	id int not null primary key identity(1,1),
 	marka varchar(50),
-	sifra char(8),
-	spol bit
+	korisnik int
+);
+
+create table popravak(
+	id int not null primary key identity(1,1),
+	segrt int,
+	trajanje int,
+	cijena decimal(18,2),
+	opis varchar(100)
+);
+
+create table popravak_sat(
+	id int not null primary key identity(1,1),
+	sat int,
+	popravak int
 );
 
 create table segrt(
-	oib char(11),
-	struka varchar(50),
+	id int not null primary key identity(1,1),
 	ime varchar(50),
-	prezime varchar(50)
+	prezime varchar(50),
+	oib char(11)
 );
+
+alter table sat add foreign key (korisnik) references korisnik (id);
+alter table popravak add foreign key (segrt) references segrt (id);
+alter table popravak_sat add foreign key (sat) references sat (id);
+alter table popravak_sat add foreign key (popravak) references popravak (id);
 
