c4f7e63b9e7a0be7e9f8ad41a0b26ee956b7c6ae
Antoniomajic <antonio.majich@gmail.com>
2023-05-16 15:23:40 +0200

    Inserted values for all tables

--- SQLvjezbe/salonzauljepsavanje.sql
+++ SQLvjezbe/salonzauljepsavanje.sql
@@ -1,8 +1,9 @@
---drop database if exists salonzauljepsavanje;
+use master;
+drop database if exists salonzauljepsavanje;
 
---create database salonzauljepsavanje;
+create database salonzauljepsavanje;
 
---use salonzauljepsavanje;
+use salonzauljepsavanje;
 
 create table djelatnica(
 	sifra int not null primary key identity(1,1),
@@ -15,7 +16,7 @@ create table korisnik(
 	sifra int not null primary key identity(1,1),
 	ime varchar(50),
 	prezime varchar(50),
-	spol bit not null
+	spol varchar(10) not null
 );
 
 create table usluga(
@@ -36,3 +37,27 @@ create table termin(
 alter table termin add foreign key (usluga) references usluga (sifra);
 alter table termin add foreign key (djelatnica) references djelatnica (sifra);
 alter table termin add foreign key (korisnik) references korisnik (sifra);
+
+insert into djelatnica (ime,prezime,oib) 
+values 
+('Matko','Valentiæ',null),
+('Silvana','Pintariæ',null),
+('Lorenc','Maroški',null);
+
+insert into  korisnik (ime,prezime,spol)
+values
+('Matko','Valentiæ','M'),
+('Silvana','Pintariæ','Ž'),
+('Lorenc','Maroški','M');
+
+insert into  usluga (naziv,cijena,trajanje)
+values
+('Pedikura','10','20'),
+('Maska za lice','10','30'),
+('Umjetni nokti','20','60');
+
+insert into termin (datum,usluga,korisnik,djelatnica) 
+values 
+('2023-05-11 9:00:00',1,2,3),
+('2023-05-11 10:00:00',3,1,2),
+('2023-05-11 11:00:00',2,3,1);
