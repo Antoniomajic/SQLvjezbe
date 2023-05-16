23fbb5f13cdda9e9b337c76aa733ce09203f5b2f
Antoniomajic <antonio.majich@gmail.com>
2023-05-16 14:57:13 +0200

    Inserted values for all tables

--- SQLvjezbe/frizerskisalon.sql
+++ SQLvjezbe/frizerskisalon.sql
@@ -15,9 +15,9 @@ create table djelatnica(
 
 create table korisnik(
 	sifra int not null primary key identity(1,1),
-	ime varchar(50),
-	prezime varchar(50),
-	spol bit not null
+	ime varchar(50) not null,
+	prezime varchar(50) not null,
+	spol varchar(10) not null
 );
 
 create table usluga(
@@ -38,3 +38,30 @@ create table termin(
 alter table termin add foreign key (usluga) references usluga (sifra);
 alter table termin add foreign key (djelatnica) references djelatnica (sifra);
 alter table termin add foreign key (korisnik) references korisnik (sifra);
+
+insert into djelatnica (ime,prezime,oib) 
+values 
+('Marija', 'Kneževiæ',null),
+('Matija', 'Aranðeloviæ',null),
+('Ivona', 'Martinoviæ',null);
+
+
+
+insert into korisnik (ime,prezime,spol) 
+values 
+('Marko','Periæ','M'),
+('Ivana','Petroviæ','Ž'),
+('Silvija','Stankoviæ','Ž');
+
+insert into usluga (naziv,cijena,trajanje)
+values
+('musko sisanje',7,20), 
+('zensko sisanje',10,35), 
+('pranje kose',5,15);
+
+insert into termin (datum,usluga,korisnik,djelatnica)
+values 
+('2023-05-10 10:00:00','1','1','1'),
+('2023-05-10 11:00:00','2','2','3'),
+('2023-05-10 10:00:00','2','3','2');
+
