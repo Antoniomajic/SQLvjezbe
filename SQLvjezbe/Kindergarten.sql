a66cb14abcd82b7180231886c585965e86e2b787
Antoniomajic <antonio.majich@gmail.com>
2023-05-18 16:36:56 +0200

    Modified Kindergarten

--- SQLvjezbe/Kindergarten.sql
+++ SQLvjezbe/Kindergarten.sql
@@ -1,82 +1,54 @@
 use master;
-drop database if exists djecjivrtic;
-create database djecjivrtic;
-use djecjivrtic;
+drop database if exists Kindergarten;
+go
+create database Kindergarten;
+go
+use Kindergarten;
 
-create table djete(
-	sifra int not null primary key identity(1,1),
-	ime varchar(50) not null,
-	prezime varchar(50) not null,
-	oib char(11)
+create table child(
+	id int not null primary key identity(1,1),
+	date_of_birth datetime not null,
+	parent_phone_number varchar(20),
+	person int not null
 );
 
-create table odgajateljica(
-	sifra int not null primary key identity(1,1),
-	ime varchar(50) not null,
-	prezime varchar(50) not null,
-	oib char(11),
-	strucnasprema int
+create table teacher(
+	id int not null primary key identity(1,1),
+	qualification int,
+	phone_number varchar (20),
+	person int not null
 );
 
-create table skupina(
-	sifra int not null primary key identity(1,1),
-	maxpolaznika int,
-	datumpocetka datetime,
-	odgajateljica int
+create table person(
+	id int not null primary key identity(1,1),
+	firstname varchar(50) not null,
+	lastname varchar(50) not null,
+	id_number char(11)
 );
 
-create table strucnasprema(
-	sifra int not null primary key identity(1,1),
-	naziv varchar(200)
+create table [group](
+	id int not null primary key identity(1,1),
+	max_children int,
+	date_of_start datetime,
+	teacher int
 );
 
-create table djete_skupina(
-	sifra int not null primary key identity(1,1),
-	djete int not null,
-	skupina int not null
+create table qualification(
+	id int not null primary key identity(1,1),
+	name varchar(200)
 );
 
-alter table odgajateljica add foreign key (strucnasprema) references strucnasprema (sifra);
-alter table skupina add foreign key (odgajateljica) references odgajateljica (sifra);
-alter table djete_skupina add foreign key (djete) references djete (sifra);
-alter table djete_skupina add foreign key (skupina) references skupina (sifra);
+create table child_group(
+	id int not null primary key identity(1,1),
+	child int not null,
+	[group] int not null
+);
 
-
-insert into strucnasprema (naziv) values ('Odgojitelj predškolske djece'), ('Pedagog');
-
-insert into odgajateljica (ime,prezime,oib,strucnasprema)
-values 
-('Ivana','Ivanoviæ',null,'1'),
-('Ivona','Ivanovska',null,'1'),
-('Filip','Filipoviæ',null,'1'),
-('Marta','Joviæ',null,'2');
-
-insert into djete (ime,prezime,oib)
-values
-('Vedrana','Bešiæ',null),
-('Nikola','Bilobrk',null),
-('Silvije','Mesariæ',null),
-('Božidar','Špiljak',null),
-('Stjepan','Kruljac',null),
-('Aleksandra','Ðujiæ',null),
-('Andrea','Tadiæ',null),
-('Gabrijela','Lekiæ',null),
-('Ivo','Malenica',null),
-('Petra','Kaliniæ',null),
-('Igor','Subotiæ',null),
-('Janica','Šajiæ',null),
-('Mislav','Olujiæ',null);
-
-insert into skupina (maxpolaznika,datumpocetka,odgajateljica)
-values 
-(10,'2023-05-10 07:00:00','1'),
-(10,'2023-05-10 07:00:00','2'),
-(10,'2023-05-10 07:00:00','3');
-
-insert into djete_skupina (djete,skupina)
-values
-('1','1'),('2','1'),('3','1'),('4','1'),
-('5','2'),('6','2'),('7','2'),('8','2'),
-('9','3'),('10','3'),('11','3'),('12','3'),('13','3');
+alter table teacher add foreign key (person) references person (id);
+alter table child add foreign key (person) references person (id);
+alter table teacher add foreign key (qualification) references qualification (id);
+alter table child_group add foreign key (child) references child (id);
+alter table child_group add foreign key ([group]) references [group] (id);
+alter table [group] add foreign key (teacher) references teacher (id);
 
 
