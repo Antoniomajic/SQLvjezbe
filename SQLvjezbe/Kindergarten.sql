9049dd8c37ee712f511c23183ae3009fa888c753
Antoniomajic <antonio.majich@gmail.com>
2023-05-18 17:04:07 +0200

    Inserted values into tables; added new ER diagram

--- SQLvjezbe/Kindergarten.sql
+++ SQLvjezbe/Kindergarten.sql
@@ -1,29 +1,26 @@
 use master;
 drop database if exists Kindergarten;
 go
-create database Kindergarten;
+create database Kindergarten collate Croatian_CI_AS;
 go
 use Kindergarten;
 
 create table child(
 	id int not null primary key identity(1,1),
-	date_of_birth datetime not null,
-	parent_phone_number varchar(20),
-	person int not null
+	firstname varchar(50) not null,
+	lastname varchar(50) not null
+);
+
+create table qualification(
+	id int not null primary key identity(1,1),
+	name varchar(200)
 );
 
 create table teacher(
 	id int not null primary key identity(1,1),
-	qualification int,
-	phone_number varchar (20),
-	person int not null
-);
-
-create table person(
-	id int not null primary key identity(1,1),
 	firstname varchar(50) not null,
 	lastname varchar(50) not null,
-	id_number char(11)
+	qualification int 
 );
 
 create table [group](
@@ -33,10 +30,6 @@ create table [group](
 	teacher int
 );
 
-create table qualification(
-	id int not null primary key identity(1,1),
-	name varchar(200)
-);
 
 create table child_group(
 	id int not null primary key identity(1,1),
@@ -44,11 +37,59 @@ create table child_group(
 	[group] int not null
 );
 
-alter table teacher add foreign key (person) references person (id);
-alter table child add foreign key (person) references person (id);
 alter table teacher add foreign key (qualification) references qualification (id);
 alter table child_group add foreign key (child) references child (id);
 alter table child_group add foreign key ([group]) references [group] (id);
 alter table [group] add foreign key (teacher) references teacher (id);
 
+insert into qualification (name)
+	values 
+			('Kindergarten Teacher'),
+			('Pedagogue')
 
+
+insert into teacher (firstname,lastname,qualification) 
+	values
+			('Elizabeta','Majcen','1'),
+			('Blaženka','Stojkoviæ','1'),
+			('Željka','Fišter','2');
+
+insert into child (firstname,lastname)
+	values
+			('Šime','Josipoviæ'),
+			('David','Matijaševiæ'),
+			('Ilija','Kramer'),
+			('Josipa','Jakoviæ'),
+			('Žarko','Volarev'),
+			('Viktorija','Ženiæ'),
+			('Draženka','Kozul'),
+			('Nikola','Kataviæ'),
+			('Branko','Iliæ'),
+			('Lucija','Krizmaniæ'),
+			('Aleksandar','Bubiæ'),
+			('Oliver','Lovrenèiæ'),
+			('Ivana','Repar');
+
+insert into [group] (max_children,date_of_start,teacher)
+	values
+			(10,'2023-01-05 07:00:00',1),
+			(10,'2023-01-05 07:00:00',2),
+			(10,'2023-01-05 07:00:00',3);
+
+insert into child_group (child,[group])
+	values	
+			(1,1),
+			(2,1),
+			(3,1),
+			(4,1),
+			(5,1),
+			(6,2),
+			(7,2),
+			(8,2),
+			(9,2),
+			(10,3),
+			(11,3),
+			(12,3),
+			(13,3);
+
+update qualification set name='Assistant Kindergarten Teacher' where id=2;
