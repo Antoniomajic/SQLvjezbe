c4fcde784c110ef18d732daac595363099a14c8b
Antoniomajic <antonio.majich@gmail.com>
2023-05-19 20:27:46 +0200

    Modified Faculty.sql & added new ER diagram

--- SQLvjezbe/Faculty.sql
+++ SQLvjezbe/Faculty.sql
@@ -1,36 +1,69 @@
 use master;
---drop database if exists fakultet;
-create database fakultet;
-use fakultet;
+drop database if exists Faculty;
+create database Faculty collate Croatian_CI_AS;
+go
+use Faculty;
 
 create table student(
-	sifra int not null primary key identity(1,1),
-	ime varchar(50) not null,
-	prezime varchar(50) not null,
-	jmbag char(11)
+	id int not null primary key identity(1,1),
+	firstname varchar(50) not null,
+	lastname varchar(50) not null,
+	id_number char(11)
 );
 
-create table kolegij(
-	sifra int not null primary key identity(1,1),
-	naziv varchar(100),
-	opis varchar (200)
+create table course(
+	id int not null primary key identity(1,1),
+	name varchar(100),
+	description varchar (200)
 );
 
-create table rok(
-	sifra int not null primary key identity(1,1),
-	vrijeme datetime,
-	kolegij int
+create table term(
+	id int not null primary key identity(1,1),
+	date datetime,
+	course int
 );
 
-create table student_rok(
-	sifra int not null primary key identity(1,1),
+create table student_term(
+	id int not null primary key identity(1,1),
 	student int not null,
-	rok int not null
+	term int not null
 );
 
-alter table student_rok add foreign key (student) references student (sifra);
-alter table student_rok add foreign key (rok) references rok (sifra);
-alter table rok add foreign key (kolegij) references kolegij (sifra);
+alter table student_term add foreign key (student) references student (id);
+alter table student_term add foreign key (term) references term (id);
+alter table term add foreign key (course) references course (id);
 
+insert into student (firstname,lastname,id_number)
+	values
+			('Zlatko','Panjiæ','21378764202'),
+			('Emil','Hadžiæ','71346581854'),
+			('Duje','Ivanoviæ','50484497824'),
+			('Ivana','Bogdan','94359048825'),
+			('Manuela','Mitroviæ','53835662727');
 
+insert into course (name,description)
+	values	
+			('Osnove ekonomije 1','Prvi kolegij iz ekonomije;osnove ekonomske misli, temeljna ekonomska naèela'),
+			('Raèunovodstvo 1','Drugi kolegij iz ekonomije; teorijsko i praktièno znanje iz podruèja raèunovodstva');
 
+insert into term (date,course)
+	values 
+			('2023-03-15 09:00:00','1'),
+			('2023-03-28 12:00:00','1'),
+			('2023-04-07 09:00:00','2'),
+			('2023-04-21 09:00:00','2');
+
+insert into student_term (student,term)
+	values
+			('1','1'),
+			('1','2'),
+			('2','1'),
+			('2','2'),
+			('3','1'),
+			('3','2'),
+			('4','1'),
+			('4','2'),
+			('5','1'),
+			('5','2');
+
+update student set firstname='Ivona' where id=4;
