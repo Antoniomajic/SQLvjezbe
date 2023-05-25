dbe0501b714f4e3e03b6efb1434bd94da6b67da9
Antoniomajic <antonio.majich@gmail.com>
2023-05-25 17:39:35 +0200

    Modified

--- SQLvjezbe/Plumber.sql
+++ SQLvjezbe/Plumber.sql
@@ -1,36 +1,49 @@
---create database vodoinstalater;
---use vodoinstalater;
+use master;
+drop database if exists Plumber;
+create database Plumber;
+go
+use Plumber;
 
-create table vodoinstalaterskiobrt(
-	iban char(21),
-	telefon varchar(20),
-	email varchar(100)
+create table failure(
+	id int not null primary key identity(1,1),
+	name varchar(50),
+	description varchar(50)
 );
 
-create table vodoinstalater(
-	ime varchar(50),
-	prezime varchar(50),
-	oib char(11),
-	registriran bit,
+create table repair(
+	id int not null primary key identity(1,1),
+	description varchar(50),
+	price decimal(18,2)
 );
 
-create table segrt(
-	ime varchar(50),
-	prezime varchar(50),
-	oib char(11)
+create table apprentice(
+	id int not null primary key identity(1,1),
+	firstname varchar(50),
+	lastname varchar(50)
 );
 
-create table kvar(
-	naziv varchar(50),
-	opis varchar(200),
-	trajanjepopravka int,
-	cijenapopravka decimal(18,2)
+create table appointment(
+	id int not null primary key identity(1,1),
+	date datetime, 
+	repair int,
+	failure int,
+	apprentice int
 );
 
-create table popravak(
-	broj varchar(50),
-	trajanje int, 
-	sudjelovanjesegrta bit,
-	cijena decimal(18,2),
-);
+alter table appointment add foreign key (repair) references repair (id);
+alter table appointment add foreign key (failure) references failure (id);
+alter table appointment add foreign key (apprentice) references apprentice (id);
+
+insert into repair (description,price)
+	values 
+			('Leakage','50');
+
+insert into failure (name,description)
+	values
+			('Rusty pipe','Rust deteriorated pipe quality');
+
+insert into apprentice (firstname,lastname)
+	values
+			('John','Sterling');
+
 
