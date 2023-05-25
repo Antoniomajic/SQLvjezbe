b0bc59e16b5e155d7f98b9683dcc401a0c702b7c
Antoniomajic <antonio.majich@gmail.com>
2023-05-25 16:43:13 +0200

    Made changes

--- SQLvjezbe/Restaurant.sql
+++ SQLvjezbe/Restaurant.sql
@@ -1,26 +1,58 @@
---create database restoran;
---use restoran;
+use master;
+drop database if exists Restaurant;
+go
+create database Restaurant;
+go
+use Restaurant;
 
-create table restoran(
-	naziv varchar(50),
-	adresa varchar(100),
-	telefon varchar(20)
+create table category(
+	id int not null primary key identity(1,1),
+	name varchar(50) not null
 );
 
-create table pice(
-	naziv varchar(50),
-	opis varchar(50),
-	cijena decimal(18,2)
+create table meal(
+	id int not null primary key identity(1,1),
+	name varchar(100),
+	description varchar(100),
+	category int
+);
+ create table drink(
+	id int not null primary key identity(1,1),
+	name varchar(100),
+	decription varchar(100)
+ );
+
+ create table drink_meal(
+	id int not null primary key identity(1,1),
+	drink int,
+	meal int
 );
 
-create table jelo(
-	naziv varchar(50),
-	opis varchar(100),
-	cijena decimal(18,2)
-);
+alter table meal add foreign key (category) references category(id);
+alter table drink_meal add foreign key (drink) references drink(id);
+alter table drink_meal add foreign key (meal) references meal(id);
 
-create table kategorija(
-	naziv varchar(50),
-	opis varchar(100)
-);
+insert into category (name)
+	values
+			('Fish meals'),
+			('Meat meals'),
+			('Veggie meals');
+
+insert into meal (name,description,category)
+	values 
+			('Baracuda mix','Baracuda,salmon and rice','1'),
+			('Full plate','Mix of different meats','2'),
+			('Garden on table','Salad','3');
+
+insert into drink (name,decription)	
+	values 
+			('Chardonnay','White wine'),
+			('Merlot','Red wine'),
+			('Coca-cola','Soda');
+
+
+
+
+
+
 
