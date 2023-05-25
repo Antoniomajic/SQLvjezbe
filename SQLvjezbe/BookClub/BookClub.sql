95e73cacec55737f7852718ad6bc387765c2caa0
Antoniomajic <antonio.majich@gmail.com>
2023-05-25 20:42:42 +0200

    Modified/replaced

--- SQLvjezbe/BookClub/BookClub.sql
+++ SQLvjezbe/BookClub/BookClub.sql
@@ -1,24 +1,62 @@
---create database klubcitatelja;
+use master;
+drop database if exists BookCLub;
+go
+create database BookCLub;
+go
+use BookCLub;
 
---use klubcitatelja;
-
-create table klub(
-	facebookgrupa varchar(200),
+create table member(
+	id int not null primary key identity(1,1),
+	firstname varchar(50),
+	lastname varchar(50),
+	id_number char(11)
 );
 
-create table citatelj(
-	ime varchar(50),
-	prezime varchar(50),
-	email varchar(100)
+create table book_owner(
+	id int not null primary key identity(1,1),
+	member int
 );
 
-create table knjiga(
-	naziv varchar(200),
-	sifra varchar(20),
-	izdanje int,
-	dostupnost bit
+create table book(
+	id int not null primary key identity(1,1),
+	book_owner int,
+	title varchar(50),
+	author varchar(200)
 );
 
-create table vlasnikknjige(
-	sifra char(4)
+create table member_book(
+	id int not null primary key identity(1,1),
+	member int,
+	book int
 );
+
+alter table book_owner add foreign key (member) references member (id);
+alter table member_book add foreign key (member) references member (id);
+alter table member_book add foreign key (book) references book (id);
+alter table book add foreign key (book_owner) references book_owner (id);
+
+insert into member (firstname,lastname,id_number)
+	values
+			('Alfred','Richardson','48874800397'),
+			('Cooper','Cole','26986714984'),
+			('Martha','Carr','97977621965');
+
+insert into book_owner (member)
+	values
+			('1'),
+			('2'),
+			('3');
+
+insert into book (book_owner,title,author)
+	values
+			('1','The Girl','Hugh Michaels'),
+			('2','Mask','Marry Carrick'),
+			('2','Six Crows','Leigh Bardugo'),
+			('3','Blood','Eleanor Shearer');
+
+update book set author='Matt Noble' where id=2;
+
+
+
+
+
