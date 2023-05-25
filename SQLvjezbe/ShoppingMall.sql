a98a1cba239823118d3033491bb28b689ddedee8
Antoniomajic <antonio.majich@gmail.com>
2023-05-25 16:14:28 +0200

    Modified

--- SQLvjezbe/ShoppingMall.sql
+++ SQLvjezbe/ShoppingMall.sql
@@ -1,30 +1,50 @@
---create database trgovackicentar;
---use trgovackicentar;
+use master;
+drop database if exists ShoppingMall;
+go
+create database ShoppingMall;
+go
+use ShoppingMall;
 
-create table trgovackicentar(
-	naziv varchar(50),
-	adresa varchar(50),
-	email varchar(100)
+create table store(
+	id int not null primary key identity(1,1),
+	name varchar(50) not null,
+	opens time,
+	closes time
 );
 
-create table trgovina(
-	naziv varchar(50),
-	iban char(21),
-	opis varchar(100)
+create table employee(
+	id int not null primary key identity(1,1),
+	firstname varchar(50) not null,
+	lastname varchar(50) not null
 );
 
-create table osoba(
-	ime varchar(50),
-	prezime varchar(50),
-	oib char(11)
+create table store_employee(
+	id int not null primary key identity(1,1),
+	store int,
+	employee int 
 );
 
-create table seftrgovine(
-	telefon varchar(20),
-	email varchar(100)
+create table boss(
+	id int not null primary key identity(1,1),
+	employee int,
+	store int
 );
 
-create table djelatniktrgovine(
-	telefon varchar(20),
-	email varchar(20)
-);
+alter table store_employee add foreign key (store) references store (id);
+alter table store_employee add foreign key (employee) references employee(id);
+alter table boss add foreign key (employee) references employee(id);
+alter table boss add foreign key (store) references store (id);
+
+insert into employee (firstname,lastname)
+	values
+			('Mark','Collins'),
+			('Jason','Palmer'),
+			('Ewan','Watson'),
+			('Anna','Brooks');
+
+insert into store (name,opens,closes)
+	values
+			('Adidas','09:00:00','21:00:00'),
+			('Deichman','08:00:00','20:00:00'),
+			('H&M','07:00:00','22:00:00');
+
