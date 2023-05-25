c6623b4f79d68a8670575a731ea7996682b01c08
Antoniomajic <antonio.majich@gmail.com>
2023-05-25 15:40:50 +0200

    Modified HighSchool

--- SQLvjezbe/HighSchool.sql
+++ SQLvjezbe/HighSchool.sql
@@ -1,30 +1,70 @@
---create database srednjaskola;
---use srednjaskola;
+use master;
+drop database if exists HighSchool;
+go
+create database HighSchool collate Croatian_CI_AS;
+go
+use HighSchool;
 
-create table skola(
-	naziv varchar(50),
-	gimnazija bit,
-	adresa varchar(50)
+create table student(
+	id int not null primary key identity(1,1),
+	firstname varchar(50),
+	lastname varchar(50),
+	sex char(1),
+	class int
 );
 
-create table ucenik(
-	ime varchar(50),
-	prezime varchar(50),
-	godinarodenja char(4),
-	spol bit
+create table professor(
+	id int not null primary key identity(1,1),
+	firstname varchar(50),
+	lastname varchar(50),
+	id_number char(11)
 );
 
-create table profesor(
-	ime varchar(50),
-	prezime varchar(50),
-	certificiran bit
+create table class(
+	id int not null primary key identity(1,1),
+	name varchar(3),
+	maxstudents varchar(2)
 );
 
-create table razred (
-	broj int,
-	odijeljenje char(2),
-	maxucenika int
+create table class_professor(
+	id int not null primary key identity(1,1),
+	class int not null,
+	professor int not null
 );
 
+alter table student add foreign key (class) references class (id);
+alter table class_professor add foreign key (class) references class (id);
+alter table class_professor add foreign key (professor) references professor (id);
 
+select * from professor;
+
+insert into class (name,maxstudents)
+	values
+			('1.A','20'),
+			('1.B','22'),
+			('1.C','21');
+
+insert into student (firstname,lastname,sex,class)
+	values
+			('Matej','Kokot','M','1'),
+			('Dinko','Frijang','M','1'),
+			('Zdravko','Krajinoviæ','M','2'),
+			('Marija','Džaja','Ž','3');
+
+insert into professor (firstname,lastname,id_number)
+	values
+			('Matija','Braut','91064433687'),
+			('Ante','Župan','14082055968'),
+			('Edita','Panjiæ','79628991552'),
+			('Manuela','Rak','56002245214');
+
+insert into class_professor (class,professor)
+	values
+			('1','1'),
+			('1','2'),
+			('2','2'),
+			('3','3');
+
+update professor set firstname='Boris' where id=1;
+	
 
