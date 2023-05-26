9b733539e6f5d4c714f278f467ebdf7a0874d30f
Antoniomajic <antonio.majich@gmail.com>
2023-05-26 17:10:42 +0200

    Modified

--- SQLvjezbe/Selfgovernment/SelfGovernment.sql
+++ SQLvjezbe/Selfgovernment/SelfGovernment.sql
@@ -1,31 +1,35 @@
-create database samouprava;
+use master;
+drop database if exists SelfGovernment;
+go
+create database SelfGovernment;
+go
+use SelfGovernment;
 
-use samouprava;
 
-create table zupanija(
-	sifra int not null primary key identity(1,1),
-	naziv varchar(100) not null,
-	zupan int
+create table county(
+	id int not null primary key identity(1,1),
+	name varchar(100) not null,
+	district_mayor int
 );
 
-create table zupan(
-	sifra int not null primary key identity(1,1),
-	ime varchar(50),
-	prezime varchar(50)
+create table district_mayor(
+	id int not null primary key identity(1,1),
+	firstname varchar(50),
+	lastname varchar(50)
 );
 
-create table opcina(
-	sifra int not null primary key identity(1,1),
-	zupanija int not null,
-	naziv varchar(50) not null
+create table municipality(
+	id int not null primary key identity(1,1),
+	county int not null,
+	name varchar(50) not null
 );
 
-create table mjesto(
-	sifra int not null primary key identity(1,1),
-	opcina int not null,
-	naziv varchar(50)
+create table town(
+	id int not null primary key identity(1,1),
+	municipality int not null,
+	name varchar(50)
 );
 
-alter table zupanija add foreign key (zupan) references zupan (sifra);
-alter table mjesto add foreign key (opcina) references opcina (sifra);
-alter table opcina add foreign key (zupanija) references zupanija (sifra);
+alter table county add foreign key (district_mayor) references district_mayor (id);
+alter table town add foreign key (municipality) references municipality (id);
+alter table municipality add foreign key (county) references county (id);
