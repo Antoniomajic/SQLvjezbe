38933f27dfb1857d65b8527330e22aaf77d19791
Antoniomajic <antonio.majich@gmail.com>
2023-06-15 14:17:52 +0200

    HelloWorld program

---
+++ CSHARP/Console01/ConsoleApp01/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Antonio Majić");
+            Console.WriteLine("Edunova");
+        }
+    }
+}
---
+++ CSHARP/Console01/ConsoleApp02/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.Write("Unesi ime grada: ");
+
+
+string ime = Console.ReadLine();
+
+Console.WriteLine(ime);
