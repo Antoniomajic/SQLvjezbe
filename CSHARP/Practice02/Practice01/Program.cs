3aa5a4e3a55d6212689fd41b21e9a19d39232fd4
Antoniomajic <antonio.majich@gmail.com>
2023-06-17 14:45:53 +0200

    Made changes

---
+++ CSHARP/Practice02/Practice01/Program.cs
@@ -0,0 +1,7 @@
+﻿const float Pi = 3.14f;
+Console.WriteLine("Insert a number: ");
+double r = Double.Parse(Console.ReadLine());
+double pi = 3.14;
+double o = 2 * r * pi;
+double p = r * r * pi;
+Console.WriteLine("o={0}, p={1}", o, p);
--- CSHARP/Practice02/Practice02/Program.cs
+++ CSHARP/Practice02/Practice02/Program.cs
@@ -4,32 +4,28 @@
     { 
          static void Main(string[] args)
            {
-            //Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World");
 
-            //byte number = 2;
-            //int count = 10;
-            //float totalPrice = 20.95f;
-            //char character = 'A';
-            //string firstName = "Mosh";
-            //bool isWorking = false;
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "Mosh";
+            bool isWorking = false;
 
-            //Console.WriteLine(number);
-            //Console.WriteLine(count);
-            //Console.WriteLine(totalPrice);
-            //Console.WriteLine(character);
-            //Console.WriteLine(firstName);
-            //Console.WriteLine(isWorking);
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
 
-            //Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
-            //Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
 
-            //const float Pi = 3.14f;
-            Console.WriteLine("Insert a number: ");
-            double r = Double.Parse(Console.ReadLine());
-            double pi = 3.14;
-            double o = 2 * r * pi;
-            double p = r * r * pi;
-            Console.WriteLine("o={0}, p={1}",o,p);
+            
+
+
 
             
         }
