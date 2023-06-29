d18fedf6eb8c92b108d78d88284e485ca399a35c
Antoniomajic <antonio.majich@gmail.com>
2023-06-29 19:44:05 +0200

    Created Practice04

---
+++ CSHARP/Practice04/Practice04/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] luckyNumbers = { 1, 4, 554, 63 };
+
+luckyNumbers[3] = 457;
+
+Console.WriteLine(luckyNumbers[3]);
+
+
+string[] friend = new string[5];
+friend[0] = "Jim";
+friend[1] = "Kely";
+friend[2] = "Marc";
+
+Console.WriteLine("**********************************");
+
+
