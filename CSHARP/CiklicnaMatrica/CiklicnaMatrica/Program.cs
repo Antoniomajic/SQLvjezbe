35d33a9ecc7e2305d59ad018ad2d7ff6b88c8dea
Antoniomajic <antonio.majich@gmail.com>
2023-07-01 14:50:40 +0200

    Modified

--- CSHARP/CiklicnaMatrica/CiklicnaMatrica/Program.cs
+++ CSHARP/CiklicnaMatrica/CiklicnaMatrica/Program.cs
@@ -13,7 +13,7 @@ for (int i = 1; i <= stupaca; i++)
     matrica[redaka - 1, stupaca - i] = b++;
 }
 
-for (int i = 0; i < redaka; i++) // ovo je uvijek isto
+for (int i = 0; i < redaka; i++)
 {
     for (int j = 0; j < stupaca; j++)
     {
@@ -21,15 +21,13 @@ for (int i = 0; i < redaka; i++) // ovo je uvijek isto
     }
     Console.WriteLine();
 }
-
-Console.WriteLine("****************************");
+Console.WriteLine("****************************1");
 
 // drugi red (stupaca od dolje prema gore)
 for (int i = redaka - 2; i >= 0; i--)
 {
     matrica[i, 0] = b++;
 }
-
 
 for (int i = 0; i < redaka; i++)
 {
@@ -39,6 +37,39 @@ for (int i = 0; i < redaka; i++)
     }
     Console.WriteLine();
 }
-Console.WriteLine("****************************");
+Console.WriteLine("****************************2");
 
-// treći red (redaka ka više)
+
+// treći red (krećemo od matrica[0,1] i idemo sa lijeva na desno)
+for (int i = 1; i <= stupaca - 1; i++)
+{
+    matrica[0, i] = b++;
+}
+
+for (int i = 0; i < redaka; i++)
+{
+    for (int j = 0; j < stupaca; j++)
+    {
+        Console.Write(matrica[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("****************************3");
+
+// 4. idemo od zadnjeg stupca do dolje
+for (int i = 1; i <= redaka - 2; i++)
+{
+    matrica[i, stupaca - 1] = b++;
+}
+
+for (int i = 0; i < redaka; i++)
+{
+    for (int j = 0; j < stupaca; j++)
+    {
+        Console.Write(matrica[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("****************************4");
+//treba dovršiti
+
