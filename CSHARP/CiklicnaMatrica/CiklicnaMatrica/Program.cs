b8b6df4af080d0ae9703cb537a52a5ccf15692d5
Antoniomajic <antonio.majich@gmail.com>
2023-07-11 18:57:40 +0200

    Modified

--- CSHARP/CiklicnaMatrica/CiklicnaMatrica/Program.cs
+++ CSHARP/CiklicnaMatrica/CiklicnaMatrica/Program.cs
@@ -1,78 +1,62 @@
-﻿Console.WriteLine("Unesi broj redaka: ");
-int redaka = int.Parse(Console.ReadLine());
+﻿int redaka, stupaca;
 
-Console.WriteLine("Unesi broj stupaca: ");
-int stupaca = int.Parse(Console.ReadLine());
+Console.WriteLine("Welcome to the program!");
+
+Console.Write("Please insert the number of rows: ");
+redaka = int.Parse(Console.ReadLine());
+
+Console.Write("Now insert the number of columns: ");
+stupaca = int.Parse(Console.ReadLine());
+
+Console.WriteLine("*********************************");
 
 int[,] matrica = new int[redaka, stupaca];
 int b = 1; //brojač
 string s;
 
-// prvi red (redaka od iza)
-for (int i = 1; i <= stupaca; i++)
-{
-    matrica[redaka - 1, stupaca - i] = b++;
-}
 
-for (int i = 0; i < redaka; i++)
-{
-    for (int j = 0; j < stupaca; j++)
+
+    for (int i = 1; i <= stupaca; i++)
     {
-        Console.Write(matrica[i, j] + " ");
+        matrica[redaka - 1, stupaca - i] = b++;
     }
-    Console.WriteLine();
-}
-Console.WriteLine("****************************1");
 
-// drugi red (stupaca od dolje prema gore)
-for (int i = redaka - 2; i >= 0; i--)
-{
-    matrica[i, 0] = b++;
-}
 
-for (int i = 0; i < redaka; i++)
-{
-    for (int j = 0; j < stupaca; j++)
+    //2
+    for (int i = redaka - 2; i >= 0; i--)
     {
-        Console.Write(matrica[i, j] + " ");
+        matrica[i, 0] = b++;
     }
-    Console.WriteLine();
-}
-Console.WriteLine("****************************2");
 
 
-//// treći red (krećemo od matrica[0,1] i idemo sa lijeva na desno)
-for (int i = 1; i <= stupaca - 1; i++)
-{
-    matrica[0, i] = b++;
-}
-
-for (int i = 0; i < redaka; i++)
-{
-    for (int j = 0; j < stupaca; j++)
+    //3
+    for (int i = 1; i <= stupaca - 1; i++)
     {
-        Console.Write(matrica[i, j] + " ");
+        matrica[0, i] = b++;
     }
-    Console.WriteLine();
-}
-Console.WriteLine("****************************3");
 
-//// 4. idemo od zadnjeg stupca do dolje
-for (int i = 1; i <= redaka - 2; i++)
-{
-    matrica[i, stupaca - 1] = b++;
-}
-
-for (int i = 0; i < redaka; i++)
-{
-    for (int j = 0; j < stupaca; j++)
+    //4
+    for (int i = 1; i <= redaka - 2; i++)
     {
-        Console.Write(matrica[i, j] + " ");
+        matrica[i, stupaca - 1] = b++;
+
     }
-    Console.WriteLine();
-}
-Console.WriteLine("****************************4");
-//treba dovršiti
+
+    // tablica
+    for (int i = 0; i < redaka; i++)
+    {
+        for (int j = 0; j < stupaca; j++)
+        {
+            s = "    " + matrica[i, j];
+            Console.Write(s[^4..]);
+        }
+        Console.WriteLine();
+    }
+
+    
+
+
+
 
 
 
