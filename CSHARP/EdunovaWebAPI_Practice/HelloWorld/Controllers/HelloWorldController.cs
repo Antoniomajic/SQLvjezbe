7d43505c5b905f3951e4728c80c2c173a76dc59d
Antoniomajic <antonio.majich@gmail.com>
2023-09-14 14:36:15 +0200

    New code additions

--- CSHARP/EdunovaWebAPI_Practice/HelloWorld/Controllers/HelloWorldController.cs
+++ CSHARP/EdunovaWebAPI_Practice/HelloWorld/Controllers/HelloWorldController.cs
@@ -62,7 +62,65 @@ namespace HelloWorld.Controllers
         // Kreirati rutu /HelloWorld/ciklicna
         // koja prima dva parametra (x i y) a vraća cikličnu matricu kao dvodimenzionalni niz brojeva
 
+        [HttpGet]
+        [Route("Ciklična")]
+        public int[,] CiklicnaMatrica(int redaka, int stupaca)
+        {
+          
 
+            int[,] matrica = new int[redaka, stupaca];
+            int b = 1; //brojač
+            string s;
+            int a = 0;
+
+
+
+            while (b <= redaka * stupaca)
+            {
+                //1
+                for (int i = a + 1; i <= stupaca - a; i++)
+                {
+                    matrica[redaka - a - 1, stupaca - i] = b++;
+                }
+
+
+                //2
+                for (int i = redaka - a - 2; i >= a; i--)
+                {
+                    matrica[i, a] = b++;
+                }
+
+
+                //3
+                for (int i = a + 1; i <= stupaca - a - 1; i++)
+                {
+                    matrica[a, i] = b++;
+                }
+
+                //4
+                for (int i = a + 1; i <= redaka - a - 2; i++)
+                {
+                    matrica[i, stupaca - a - 1] = b++;
+
+                }
+
+
+                a++;
+            }
+            // tablica
+            for (int i = 0; i < redaka; i++)
+            {
+                for (int j = 0; j < stupaca; j++)
+                {
+                    s = "    " + matrica[i, j];
+                    Console.Write(s[^4..]);
+                }
+                Console.WriteLine();
+            }
+
+
+            return matrica;
+        }
 
 
 
