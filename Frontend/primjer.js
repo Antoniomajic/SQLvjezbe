143d42139dd8682a509f414d98851708ce9f4e63
Antoniomajic <antonio.majich@gmail.com>
2023-09-26 22:19:11 +0200

    Hello world

---
+++ Frontend/primjer.js
@@ -0,0 +1,14 @@
+/*alert('Hello world')  može se i dvostruke navodnike koristiti */
+
+// document.write('Hello world');
+
+console.log('Hello world');
+
+for(i = 0; i <100 ; i++){
+    console.log('Osijek' + i);
+}
+
+let ime = "Pero";
+console.log(typeof(ime));
+
+ime = 10;
