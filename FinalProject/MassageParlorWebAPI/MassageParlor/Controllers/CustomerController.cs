f99d3c37d741e9c15bb828315129cae103f3629e
Antoniomajic <antonio.majich@gmail.com>
2023-10-02 15:51:45 +0200

    Added documentation

--- FinalProject/MassageParlorWebAPI/MassageParlor/Controllers/CustomerController.cs
+++ FinalProject/MassageParlorWebAPI/MassageParlor/Controllers/CustomerController.cs
@@ -4,6 +4,10 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MassageParlor.Controllers
 {
+    /// <summary>
+    /// Intended for CRUD operations on entity Customer in database
+    /// </summary>
+
     [ApiController]
     [Route("api/v1/[controller]")]
     public class CustomerController : ControllerBase
@@ -15,12 +19,38 @@ namespace MassageParlor.Controllers
             _context = context;
         }
 
+        /// <summary>
+        /// Gets(pulls) all customers from database 
+        /// </summary>
+        /// <remarks>
+        /// Example query:
+        /// 
+        ///     GET api/v1/Customer
+        /// </remarks>
+        /// <returns>Customer in database</returns>
+        /// <response code="200">Everything is OK</response>
+        /// <response code="400">Request is not valid (BadRequest)</response>
+        /// <response code="503">Add IP in firewall on azure</response>        
         [HttpGet]
         public IActionResult Get()
         {           
             return new JsonResult(_context.Customer.ToList());
         }
 
+        /// <summary>
+        /// Adds customer in database
+        /// </summary>
+        /// <remarks>
+        /// Query example:
+        /// 
+        ///     POST api/v1/Customer
+        ///     {firstname:"name",lastname:"surname"}
+        ///     
+        /// </remarks>
+        /// <returns>Created customer in database with all information</returns>
+        /// <response code="200">Everything is OK</response>
+        /// <response code="400">Request is not valid (BadRequest)</response> 
+        /// <response code="503">Add IP in firewall on azure</response> 
         [HttpPost]
         public IActionResult Post(Customer customer)
         {
--- FinalProject/MassageParlorWebAPI/MassageParlor/Program.cs
+++ FinalProject/MassageParlorWebAPI/MassageParlor/Program.cs
@@ -1,5 +1,6 @@
 using MassageParlor.Data;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +9,28 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(sgo =>
+{
+    var o = new Microsoft.OpenApi.Models.OpenApiInfo()
+    {
+        Title = "Massage Parlor API",
+        Version = "v1",
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact()
+        {
+            Email = "antonio.majich@gmail.com",
+            Name = "Antonio Majiæ"
+        },
+        Description = "This is documentation for Massage Parlor API",
+        License = new Microsoft.OpenApi.Models.OpenApiLicense()
+        {
+            Name = "Educational license"
+        }
+    };
+    sgo.SwaggerDoc("v1", o);
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    sgo.IncludeXmlComments(xmlPath);
+});
 
 builder.Services.AddDbContext<MassageParlorContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString(name: "MassageParlorContext")));
 
